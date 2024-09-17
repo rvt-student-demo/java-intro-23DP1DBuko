a17100e62633b11431a29958218ef9dac829228b
23DP1DBuko <166384453+23DP1DBuko@users.noreply.github.com>
2024-09-17 08:07:10 +0000

    speeding ticket

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -8,29 +8,12 @@ public class App
 {
     public static void main( String[] args )
     {
-        // Izveidojam vienu reizi, un lietojam zemāk
         Scanner scanner = new Scanner(System.in);
 
-        String userInput = scanner.nextLine();
-        
-        int userInputAsInt = Integer.valueOf(userInput);
-
-        System.out.println(userInputAsInt);
-
-        int cipars1 = Integer.valueOf(scanner.nextLine());
-        int cipars2 = Integer.valueOf(scanner.nextLine());
-        int cipars3 = Integer.valueOf(scanner.nextLine());
-        
-        // izvadit to skaitļu summu
-        int summa = cipars1 + cipars2 + cipars3;
-        System.out.println("Summa: " + summa);
-
-        // izvadit to skaitļu reizinājumu
-        System.out.println("Reizinājums: " + cipars1 * cipars2 * cipars3 );
-
-        // Skaitļu un vidējo skaitļu izvade
-        int vid = summa / 3;
-        System.out.println("Vidējais aritmetiskais: " + vid);
+        int speed = Integer.valueOf(scanner.nextLine());
+        if (speed > 120) {
+            System.out.println("Speeding ticket!");
+        }
     }
 
     
