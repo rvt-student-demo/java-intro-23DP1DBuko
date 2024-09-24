febb3e1b8f409fb8bb42f01c3c6459e53b8d8d81
23DP1DBuko <166384453+23DP1DBuko@users.noreply.github.com>
2024-09-24 09:45:14 +0000

    sum of a sequence

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -11,12 +11,13 @@ public class App
     public static void main( String[] args ) {
         
         Scanner scanner = new Scanner(System.in);
-        
-        int start = Integer.valueOf(scanner.nextLine());
-        int end = 100;
-        for (int i = start; i <= end; i++) {
-            System.out.println(i);
+
+        System.out.println("Last number? ");
+        int end = Integer.valueOf(scanner.nextLine());
+        int sum = 0;
+        for (int i = 0; i <= end; i++) {
+            sum += i;
         }
-        
+        System.out.println("The sum is " + sum);
     }   
 }
