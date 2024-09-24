462719423a9496dcfcede58733051fc659ebfc50
23DP1DBuko <166384453+23DP1DBuko@users.noreply.github.com>
2024-09-24 09:22:22 +0000

    counting to hundred

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -8,24 +8,15 @@ import javax.swing.plaf.ScrollPaneUI;
 
 public class App 
 {
-    public static void main( String[] args )
-    {
-        Scanner scanner = new Scanner(System.in);
-        int amount = 0;
-        int sum = 0;
+    public static void main( String[] args ) {
         
-        while (true) {
-            System.out.println("Give a number:");
-            int number = Integer.valueOf(scanner.nextLine());
-            
-            if (number == 0) {
-                System.out.println("Number of numbers: " + amount);
-                System.out.println("Sum of the numbers: " + sum);
-                break;
-            } else {
-                amount += 1;
-                sum += number;
-            }
+        Scanner scanner = new Scanner(System.in);
+        
+        int start = Integer.valueOf(scanner.nextLine());
+        int end = 100;
+        for (int i = start; i <= end; i++) {
+            System.out.println(i);
         }
+        
     }   
 }
