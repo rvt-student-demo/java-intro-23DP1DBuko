59c9dec8896c4e1159deb4b1d683aa59c33b88c3
23DP1DBuko <166384453+23DP1DBuko@users.noreply.github.com>
2024-10-01 06:36:17 +0000

    repeating, breaking and remembering

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -10,14 +10,42 @@ public class App
 {
     public static void main( String[] args ) {
         
-        Scanner scanner = new Scanner(System.in);
+        Scanner reader = new Scanner(System.in);
 
-        System.out.println("Last number? ");
-        int end = Integer.valueOf(scanner.nextLine());
+        System.out.println("Give numbers: ");
+
         int sum = 0;
-        for (int i = 0; i <= end; i++) {
-            sum += i;
+        int validNumbers = 0;
+        int oddNumbers = 0;
+        int evenNumbers = 0;
+        
+
+        while (true) {
+            int input = Integer.valueOf(reader.nextLine());
+
+            if (input == 0) {
+                break;
+            }
+
+            if (input < 0) {
+                break;
+            }
+
+            if (input % 2 == 1) {
+                oddNumbers ++;
+            }
+            
+            if (input % 2 == 0) {
+                evenNumbers ++;
+            }
+            sum += input;
+            validNumbers++;
         }
-        System.out.println("The sum is " + sum);
+        System.out.println("Thx! Bye!");
+        System.out.println("Sum: " + sum);
+        System.out.println("Numbers: " + validNumbers);
+        System.out.println("Average: " + (sum * 1.0 / validNumbers));
+        System.out.println("Even: " + evenNumbers);
+        System.out.println("Odd: " + oddNumbers);
     }   
 }
