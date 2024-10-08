95a60cf327c862b26085d5103a75faa494f35dc0
23DP1DBuko <166384453+23DP1DBuko@users.noreply.github.com>
2024-10-08 06:57:52 +0000

    Star sign (4 parts)

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -9,9 +9,49 @@ import javax.swing.plaf.ScrollPaneUI;
 public class App 
 {
     public static void main(String[] args) {
+        System.out.println("Printing stars");
+        printStars(5);
+        printStars(3);
+        printStars(9);
+        System.out.println();
+
+        System.out.println("Printing a square");
+        printSquare(4);
+        System.out.println();
+
+        System.out.println("Printing a rectangle");
+        printRectangle(17, 3);
+        System.out.println();
+
+        System.out.println("Printing a triangle");
         printTriangle(4);
     }
     
+    public static void printStars(int number) {
+        for (int i = 0; i < number; i++) {
+            System.out.print("*");    
+        }
+        System.out.println();
+    }
+
+    public static void printSquare(int size) {
+        for (int a = 0; a < size; a++) {
+            for (int i = 0; i < size; i++) {
+                System.out.print("*");    
+            }
+            System.out.println();
+        }
+    }
+
+    public static void printRectangle(int width, int height) {
+        for (int a = 0; a < height; a++) {
+            for (int i = 0; i < width; i++) {
+                System.out.print("*");    
+            }
+            System.out.println();
+        }
+    }
+
     public static void printTriangle(int size) {
         for (int a = 0; a < size; a++) {
             for (int i = 0; i < a+1; i++) {
