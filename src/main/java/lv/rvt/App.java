690253f075e0eec67c988fcdba4bed789eb72c81
23DP1DBuko <166384453+23DP1DBuko@users.noreply.github.com>
2024-10-08 06:22:31 +0000

    print a triangle

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -9,12 +9,12 @@ import javax.swing.plaf.ScrollPaneUI;
 public class App 
 {
     public static void main(String[] args) {
-        printRectangle(17, 3);
+        printTriangle(4);
     }
     
-    public static void printRectangle(int width, int height) {
-        for (int a = 0; a < height; a++) {
-            for (int i = 0; i < width; i++) {
+    public static void printTriangle(int size) {
+        for (int a = 0; a < size; a++) {
+            for (int i = 0; i < a+1; i++) {
                 System.out.print("*");    
             }
             System.out.println();
