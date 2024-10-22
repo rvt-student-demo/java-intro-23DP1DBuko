06e558b90b6472c4cb9ada9c3673da5fea17924b
23DP1DBuko <166384453+23DP1DBuko@users.noreply.github.com>
2024-10-22 06:34:17 +0000

    only these numbers

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -7,24 +7,37 @@ import java.util.Scanner;
 import javax.swing.plaf.ScrollPaneUI;
 import java.util.*;
 
-public class App 
-{
-    public static void main(String[] args) {
-        
-        Scanner scanner = new Scanner(System.in);
-        ArrayList<Integer> intList = new ArrayList<>();
+public class App {
 
-        while (true) {
-            int input = Integer.valueOf(scanner.nextLine());
-            intList.add(input);
-            if (input == 0) {
-                break;
-            }
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        ArrayList<Integer> numbers = new ArrayList<>();
+        numbers.add(Integer.valueOf(scanner.nextLine()));
+
+        while (numbers.get(numbers.size()-1) != -1) {
+            numbers.add(Integer.valueOf(scanner.nextLine()));
         }
-        int summ = 0;
-        for(int i = 0; i<intList.size(); i++) {
-            summ += intList.get(i);
+        
+        System.out.print("From where?");
+        Integer from = Integer.valueOf(scanner.nextLine());
+
+        while (numbers.size()-1 < from || from < 0) {
+            System.out.print("From where?");
+            from = Integer.valueOf(scanner.nextLine());
         }
-        System.out.println(summ);
+
+        System.out.print("To where?");
+        Integer to = Integer.valueOf(scanner.nextLine());
+
+        while (numbers.size()-1 < to || to < 0) {
+            System.out.print("To where?");
+            to = Integer.valueOf(scanner.nextLine());
+        }
+
+        while (from <= to) {
+            System.out.println(numbers.get(from));
+            from ++;
+        }
     }
+
 }
