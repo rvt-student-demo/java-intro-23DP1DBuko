46ac711dfb7f5145c141bfe34180a019188b45d6
23DP1DBuko <166384453+23DP1DBuko@users.noreply.github.com>
2024-10-22 07:49:14 +0000

    Print in range

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -2,42 +2,34 @@ package lv.rvt;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
-import java.util.Scanner;
 
 import javax.swing.plaf.ScrollPaneUI;
 import java.util.*;
 
 public class App {
 
-    public static void main(String[] args){
-        Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args) {
         ArrayList<Integer> numbers = new ArrayList<>();
-        numbers.add(Integer.valueOf(scanner.nextLine()));
-
-        while (numbers.get(numbers.size()-1) != -1) {
-            numbers.add(Integer.valueOf(scanner.nextLine()));
-        }
+        numbers.add(3);
+        numbers.add(2);
+        numbers.add(6);
+        numbers.add(-1);
+        numbers.add(5);
+        numbers.add(1);
         
-        System.out.print("From where?");
-        Integer from = Integer.valueOf(scanner.nextLine());
-
-        while (numbers.size()-1 < from || from < 0) {
-            System.out.print("From where?");
-            from = Integer.valueOf(scanner.nextLine());
-        }
-
-        System.out.print("To where?");
-        Integer to = Integer.valueOf(scanner.nextLine());
-
-        while (numbers.size()-1 < to || to < 0) {
-            System.out.print("To where?");
-            to = Integer.valueOf(scanner.nextLine());
-        }
-
-        while (from <= to) {
-            System.out.println(numbers.get(from));
-            from ++;
-        }
+        System.out.println("The numbers in the range [0, 5]");
+        printNumbersInRange(numbers, 0, 5);
+        
+        System.out.println("The numbers in the range [3, 10]");
+        printNumbersInRange(numbers, 3, 10);
     }
 
+    public static void printNumbersInRange(ArrayList<Integer> 
+    numbers, int lowerLimit, int upperLimit) {
+        for (int number: numbers) {
+            if (lowerLimit <= number && number <= upperLimit) {
+                System.out.println(number);
+            }
+        }
+    }
 }
