fe2e146e7d02e393fe84faf51c9631ab25b28ed2
Deniss Bukovskis <166384453+23DP1DBuko@users.noreply.github.com>
2024-11-06 14:18:48 +0000

    sum and average

--- src/main/java/lv/rvt/Person.java
+++ src/main/java/lv/rvt/Person.java
@@ -3,10 +3,14 @@ package lv.rvt;
 public class Person {
     private String name;
     private int age;
+    private int weight;
+    private int height;
 
     public Person(String initialName) {
         this.age = 0;
         this.name = initialName;
+        this.weight = 0;
+        this.height = 0;
     }
 
     public void printPerson() {
@@ -18,5 +22,29 @@ public class Person {
         if (this.age < 30) {
             this.age ++;
         }
+    }
+
+    public boolean isOfLegalAge() {
+        if (this.age < 18) {
+            return false;
+        }
+        return true;
+    }
+
+    public String toString() {
+        return this.name + ", age " + this.age + " years";
+    }
+
+    public void setHeight(int newHeight) {
+        this.height = newHeight;
+    }
+
+    public void setWeight(int newWeight) {
+        this.weight = newWeight;
+    }
+
+    public double bodyMassIndex() {
+        double heigthPerHundred = this.height / 100.0;
+        return this.weight / (heigthPerHundred * heigthPerHundred);
     }
 }
---
+++ src/main/java/lv/rvt/Statistics.java
@@ -0,0 +1,33 @@
+package lv.rvt;
+
+public class Statistics {
+    private int count;
+    private int num;
+    private int sum;
+    public Statistics() {
+
+    }
+
+    public void addNumber(int number) {
+        this.num = number;
+        
+        count ++;
+        sum += this.num;
+    }
+
+    public int getCount() {
+        
+        return count;
+    }
+
+    public int sum() {
+        
+        return sum;
+        
+    }
+
+    public double average() {
+        double average = this.sum/(this.count * 1.0);
+        return average;
+    }
+}
