8449af3499d1d627da0350b9d3e24c0ed5cc835e
Deniss Bukovskis <166384453+23DP1DBuko@users.noreply.github.com>
2024-11-18 14:37:35 +0000

    Payment Card part 4

--- src/main/java/lv/rvt/PaymentCard.java
+++ src/main/java/lv/rvt/PaymentCard.java
@@ -22,4 +22,11 @@ public class PaymentCard {
             this.balance -= 4.60;
         }
     }
+
+    public void addMoney(double amount) {
+        this.balance += amount;
+        if (this.balance > 150) {
+            this.balance = 150;
+        }
+    }
 }
