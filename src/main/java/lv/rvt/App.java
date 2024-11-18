1a5ce97fdbde27fb582fb84aaf1e51f3031f12d5
Deniss Bukovskis <166384453+23DP1DBuko@users.noreply.github.com>
2024-11-18 14:43:33 +0000

    Payment Card part 6

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -8,9 +8,26 @@ import java.util.*;
 
 public class App {
     public static void main(String[] args) {
-        PaymentCard card = new PaymentCard(10);
-        System.out.println("Paul: " + card);
-        card.addMoney(-15);
-        System.out.println("Paul: " + card);
+        PaymentCard paulCard = new PaymentCard(20);
+        PaymentCard mattCard = new PaymentCard(30);
+        
+        paulCard.eatHeartily();
+        mattCard.eatAffordably();
+
+        System.out.println("Paul: " + paulCard);
+        System.out.println("Matt: " + mattCard);
+
+        paulCard.addMoney(20);
+        mattCard.eatHeartily();
+
+        System.out.println("Paul: " + paulCard);
+        System.out.println("Matt: " + mattCard);
+
+        paulCard.eatAffordably();
+        paulCard.eatAffordably();
+        mattCard.addMoney(50);
+
+        System.out.println("Paul: " + paulCard);
+        System.out.println("Matt: " + mattCard);
     }
 }
