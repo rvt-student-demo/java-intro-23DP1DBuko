120b78d37d52fa78b222fca42ee3434267ce0144
Deniss Bukovskis <166384453+23DP1DBuko@users.noreply.github.com>
2024-12-03 10:33:48 +0000

    registracija

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -2,13 +2,28 @@ package lv.rvt;
 import java.io.BufferedWriter;
 import java.nio.file.StandardOpenOption;
 import java.io.BufferedReader; 
-import java.util.*; 
+import java.util.*;
+
+import javax.sound.midi.Soundbank; 
  
 public class App { 
     public static void main(String[] args) throws Exception { 
-        Person person = new Person("Vika", 17, 50, 176);    
-    
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter your name: ");
+        String name = scanner.nextLine();
+        System.out.println("Enter your age: ");
+        int age = Integer.valueOf(scanner.nextLine());
+        System.out.println("Enter you weight: ");
+        int weight = Integer.valueOf(scanner.nextLine());
+        System.out.println("Enter your height: ");
+        int height = Integer.valueOf(scanner.nextLine());
+
+        Person person = new Person(name, age, weight, height);
+
+
         PersonManager.addPerson(person);
+        System.out.println("Paldies par reģistrāciju");
     }
     public static void modify(Person person) {
         person.setName("Modified!");
