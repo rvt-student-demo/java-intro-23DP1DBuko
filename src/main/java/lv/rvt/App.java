612673aa597886ddc788dc9b49b76507b4b061df
Deniss Bukovskis <166384453+23DP1DBuko@users.noreply.github.com>
2024-12-10 08:58:09 +0000

    Health station (2 part)

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -8,12 +8,19 @@ import javax.sound.midi.Soundbank;
  
 public class App { 
     public static void main(String[] args) {
-        // example main program for the first section of the exercise
-    
         HealthStation childrensHospital = new HealthStation();
     
         Person ethan = new Person("Ethan", 1, 7, 110);
         Person peter = new Person("Peter", 33, 85, 176);
+    
+        System.out.println(ethan.getName() + " weight: " + childrensHospital.weigh(ethan) + " kilos");
+        System.out.println(peter.getName() + " weight: " + childrensHospital.weigh(peter) + " kilos");
+    
+        childrensHospital.feed(ethan);
+        childrensHospital.feed(ethan);
+        childrensHospital.feed(ethan);
+    
+        System.out.println("");
     
         System.out.println(ethan.getName() + " weight: " + childrensHospital.weigh(ethan) + " kilos");
         System.out.println(peter.getName() + " weight: " + childrensHospital.weigh(peter) + " kilos");
--- src/main/java/lv/rvt/HealthStation.java
+++ src/main/java/lv/rvt/HealthStation.java
@@ -4,4 +4,7 @@ public class HealthStation {
     public int weigh(Person person) {
         return person.getWeight();
     }
+    public void feed(Person person) {
+        person.setWeight(person.getWeight() + 1);
+    }
 }
