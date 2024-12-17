dca488a889c23cc11ea1caac2bc92c00e36bcb3b
Deniss Bukovskis <166384453+23DP1DBuko@users.noreply.github.com>
2024-12-17 07:20:41 +0000

    data types ipasibas

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -8,20 +8,27 @@ import javax.sound.midi.Soundbank;
  
 public class App { 
     public static void main(String[] args) {
-        PaymentTerminal unicafeExactum = new PaymentTerminal();
+        // primitivie datu tipa īpašības
+        int x = 5;
+        int y = x;
+        modify(y);
+        int c = x;
+        x = 10;
+        
+        // System.out.println(c);
+        
+        // Reference datu tipu īpašības
+        Person pers1 = new Person("John");
+        Person pers2 = pers1;
+        pers1.setName("bublik");
+        modify(pers1);
+        System.out.println(pers2.getName());
+    }
+    public static void modify(Person person) {
+        person.setName("Modified!");
+    }
 
-        double change = unicafeExactum.eatAffordably(10);
-        System.out.println("remaining change: " + change);
-
-        PaymentCard annesCard = new PaymentCard(7);
-
-        boolean wasSuccessful = unicafeExactum.eatHeartily(annesCard);
-        System.out.println("there was enough money: " + wasSuccessful);
-        wasSuccessful = unicafeExactum.eatHeartily(annesCard);
-        System.out.println("there was enough money: " + wasSuccessful);
-        wasSuccessful = unicafeExactum.eatAffordably(annesCard);
-        System.out.println("there was enough money: " + wasSuccessful);
-        System.out.println(annesCard.balance());
-        System.out.println(unicafeExactum);
+    public static void modify(int value){
+        value += 5;
     }
 }
