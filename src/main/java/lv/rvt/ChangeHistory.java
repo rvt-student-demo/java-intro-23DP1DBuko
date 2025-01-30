a0655b433a2edcf60d8fd1dddcf04272fd0f412f
Deniss Bukovskis <166384453+23DP1DBuko@users.noreply.github.com>
2025-01-30 14:21:37 +0000

    not completed

---
+++ src/main/java/lv/rvt/ChangeHistory.java
@@ -0,0 +1,32 @@
+package lv.rvt;
+
+public class ChangeHistory {
+    public ChangeHistory() {
+        
+    }
+
+    public void add(double status) {
+
+    }
+
+    public void clear() {
+
+    }
+
+    @Override
+    public String toString() {
+
+    }
+
+    public double maxValue() {
+
+    }
+
+    public double minValue() {
+
+    }
+
+    public double average() {
+
+    }
+}
---
+++ src/main/java/lv/rvt/ProductWarehouseWithHistory.java
@@ -0,0 +1,12 @@
+package lv.rvt;
+
+public class ProductWarehouseWithHistory extends ProductWarehouse {
+    public ProductWarehouseWithHistory(String productName, 
+    double capacity, double initialBalance) {
+
+    }
+
+    public String history() {
+        
+    }
+}
