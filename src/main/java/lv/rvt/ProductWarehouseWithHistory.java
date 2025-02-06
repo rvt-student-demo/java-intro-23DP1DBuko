f7304dae3e1cb080fefb7612ecb0275ef65363a5
Deniss Bukovskis <166384453+23DP1DBuko@users.noreply.github.com>
2025-02-06 13:30:36 +0000

    Warehousing part 4

--- src/main/java/lv/rvt/ProductWarehouseWithHistory.java
+++ src/main/java/lv/rvt/ProductWarehouseWithHistory.java
@@ -1,12 +1,16 @@
 package lv.rvt;
 
 public class ProductWarehouseWithHistory extends ProductWarehouse {
-    public ProductWarehouseWithHistory(String productName, 
-    double capacity, double initialBalance) {
+    private ChangeHistory history;
 
+    public ProductWarehouseWithHistory(String productName, double capacity, double initialBalance) {
+        super(productName, capacity);
+        this.history = new ChangeHistory();
+        addToWarehouse(initialBalance);
+        history.add(initialBalance);
     }
 
     public String history() {
-        
+        return history.toString();
     }
 }
