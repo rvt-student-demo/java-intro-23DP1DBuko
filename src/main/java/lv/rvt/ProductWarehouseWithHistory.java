ec4d0f1bbdf5b66170460dd83c56155d24ae0df3
Deniss Bukovskis <166384453+23DP1DBuko@users.noreply.github.com>
2025-02-06 13:31:53 +0000

    warehousing part 5

--- src/main/java/lv/rvt/ProductWarehouseWithHistory.java
+++ src/main/java/lv/rvt/ProductWarehouseWithHistory.java
@@ -13,4 +13,13 @@ public class ProductWarehouseWithHistory extends ProductWarehouse {
     public String history() {
         return history.toString();
     }
+
+    public static void main(String[] args) {
+        ProductWarehouseWithHistory juice = new ProductWarehouseWithHistory("Juice", 1000.0, 1000.0);
+        juice.takeFromWarehouse(11.3);
+        System.out.println(juice.getName());
+        juice.addToWarehouse(1.0);
+        System.out.println(juice);
+        System.out.println(juice.history());
+    }
 }
